1eb24a98b19d475ab9adcd98c4edfe44247cf4ba
BiRuVN <ngngquang99@gmail.com>
2021-03-27 22:00:18 +0700

    fix layout structure

--- label_ner_characters.py
+++ label_ner_characters.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 
 root = Tk()
 root.title('Label coordinate')
-root.geometry('1024x720')
+root.geometry('650x250')
 
 # # Define dataset path and labels here
 # label_folder = './data/training/cursor/seq_01/'
@@ -18,37 +18,51 @@ target_labels = {
 
 sentence_index = 0
 
-def load_sentence(parent, sent):
-    global load
-    load = [x for x in current_sentence] + ['']*(256-len(current_sentence))
+def focus_next_widget(event):
+    event.widget.tk_focusNext().focus()
+    return 'break'
 
-    sentence_frame = Frame(parent)
+def focus_prev_widget(event):
+    event.widget.tk_focusPrev().focus()
+    return 'break'
+
+def select_all(event):
+    event.widget.tag_add(SEL, "1.0", END)
+    event.widget.mark_set(INSERT, "1.0")
+    event.widget.see(INSERT)
+    return 'break'
+
+def load_sentence(sent):
+    global load, sentence_frame
+    load = [x for x in current_sentence]
+
+    sentence_frame = Frame(height=200, width=650, bg='green')
     sentence_frame.grid(row=0, column=0)
-    # sentence_frame.pack(fill=X)
+    sentence_frame.grid_propagate(False)
     vars = []
-    i=0
+
     print(current_sentence)
     print(load)
     row = 0
     column = 0
-    for c in load:
-        column = load.index(c)+i
-        if column >= 50:
-            column = column % 50
-            row += 1
+    for i in range(len(load)):
+        column = i % 50
+        row = int(i / 50)
 
         char_frame = Frame(sentence_frame)
         char_frame.grid(row=row, column=column)
-        lbl = Label(char_frame, height=1, text=c)
+
+        lbl = Label(char_frame, height=1, width=1, text=load[i])
         var = Text(char_frame, height=1, width=1)
         lbl.grid(row=0, column=0)
         var.grid(row=1, column=0)
-        # lbl.pack(side=TOP)
-        # var.pack(side=BOTTOM)
+
         var.delete(1.0, END)
-        var.insert(1.0, '1')
+        var.insert(1.0, '0')
+        var.bind("<Tab>", focus_next_widget)
+        var.bind("<Shift-Tab>", focus_prev_widget)
+        var.bind("<FocusIn>", select_all)
         vars.append(var)
-        i += 1
 
     # with open(target_folder + current_sentence.split('.')[0] + '.txt', 'r') as label_file:
     #     r = label_file.read()
@@ -84,13 +98,13 @@ def render_sentence_holder(parent):
     # sent_lbl = Label(sentence_frame)
 
     # sent_lbl.pack(side='top')
-    check_vars = load_sentence(parent, current_sentence)
+    check_vars = load_sentence(current_sentence)
 
     progress_frame = Frame(parent)
     progress_frame.grid(row=1, column=0)
 
     progress_lbl = Label(progress_frame)
-    progress_lbl.configure(text='{} of {}'.format(sentence_index, len(sentences)))
+    progress_lbl.configure(text='{} of {}'.format(sentence_index+1, len(sentences)))
     progress_lbl.pack(side='bottom')
     
     return progress_lbl
@@ -114,86 +128,52 @@ def render_sentence_holder(parent):
 
 #     return vars
 
-# def next_image():
-#     global sentence_index, images, current_sentence, sent_lbl
-#     global img_name_lbl, progress_lbl, img_shape
-#     if sentence_index < len(images):
-#         with open(target_folder + current_sentence.split('.')[0] + '.txt', 'w') as label_file:
-#             for v in check_vars:
-#                 s = v.get(1.0, END).strip().split(' ')
-#                 label_file.write('{} {}'.format(str(s[0]), str(s[1])))
-#                 label_file.write(' ')
-
-#         sentence_index += 1
-#         current_sentence = images[sentence_index]
-#         s = load_image(current_sentence)
-#         img_shape.configure(text='shape: {}'.format(s))
+def next_sent():
+    global sentence_index, sentences, current_sentence, sent_lbl, sentence_frame
+    global progress_lbl
+    sentence_index += 1
+    if sentence_index < len(sentences):
+        # with open(target_folder + current_sentence.split('.')[0] + '.txt', 'w') as label_file:
+        #     for v in check_vars:
+        #         s = v.get(1.0, END).strip().split(' ')
+        #         label_file.write('{} {}'.format(str(s[0]), str(s[1])))
+        #         label_file.write(' ')
         
-#     else:
-#         sentence_index -= 1
+        current_sentence = sentences[sentence_index]
+        for widget in sentence_frame.winfo_children():
+            widget.destroy()
+        vars = load_sentence(sent=current_sentence)
+        vars = None
+        
+    else:
+        sentence_index -= 1
 
-#     img_name_lbl.configure(text=current_sentence)
-#     progress_lbl.configure(text='{} of {}'.format(sentence_index, len(images)))
+    progress_lbl.configure(text='{} of {}'.format(sentence_index+1, len(sentences)))
 
-# def prev_image():
-#     global sentence_index, images, current_sentence, sent_lbl
-#     global img_name_lbl, progress_lbl, img_shape
-#     sentence_index -= 1
-#     if sentence_index > -1:
-#         current_sentence = images[sentence_index]
-#         s = load_image(current_sentence)
-#         img_shape.configure(text='shape: {}'.format(s))
-#     else:
-#         sentence_index += 1
+def prev_sent():
+    global sentence_index, sentences, current_sentence, sent_lbl, sentence_frame
+    global progress_lbl
+    sentence_index -= 1
+    if sentence_index > -1:
+        current_sentence = sentences[sentence_index]
+        for widget in sentence_frame.winfo_children():
+            widget.destroy()
+        vars = load_sentence(sent=current_sentence)
+        vars = None
+    else:
+        sentence_index += 1
 
-#     img_name_lbl.configure(text=current_sentence)
-#     progress_lbl.configure(text='{} of {}'.format(sentence_index, len(images)))
+    progress_lbl.configure(text='{} of {}'.format(sentence_index+1, len(sentences)))
     
-def create_circle(x, y, color, r=3): #center coordinates, radius
-    x0 = x - r
-    y0 = y - r
-    x1 = x + r
-    y1 = y + r
-    print("draw")
-    draw = ImageDraw.Draw(load)
-    draw.ellipse([(x0, y0), (x1, y1)], fill=color)
-    render = ImageTk.PhotoImage(load)
-    sent_lbl.configure(image=render)
-    sent_lbl.image = render
-    # return draw.ellipse((x0, y0, x1, y1), fill='red', width=2)
-
-def printcoords(event, color):
-    #outputting x and y coords to console
-    x, y = event.x, event.y
-    create_circle(int(x), int(y), color)
-    return (x, y)
-
 def key_stroke(event):
     # while True:
     c = event.char
     if c == 'a':
-        prev_image()
+        prev_sent()
         # break
     elif c == 'd':
-        next_image()
+        next_sent()
         # break
-    else:
-        print(c)
-        if c == '1':
-            check_vars[0].delete(1.0, END)
-            check_vars[0].insert(1.0, printcoords(event, 'red'))
-        elif c == '2':
-            check_vars[1].delete(1.0, END)
-            check_vars[1].insert(1.0, printcoords(event, 'blue'))
-        elif c == '3':
-            check_vars[2].delete(1.0, END)
-            check_vars[2].insert(1.0, printcoords(event, 'green'))
-        elif c == '4':
-            check_vars[3].delete(1.0, END)
-            check_vars[3].insert(1.0, printcoords(event, 'yellow'))
-        elif c == '5':
-            check_vars[4].delete(1.0, END)
-            check_vars[4].insert(1.0, printcoords(event, 'black'))
 
 # get all sentences that need to be labeled
 sentences = []
@@ -216,7 +196,7 @@ if not os.path.exists(label_path):
 current_sentence = sentences[sentence_index]
 sent_lbl = None
 load = None
-# check_vars = load_sentence(current_sentence)
+vars = load_sentence(current_sentence)
 progress_lbl = render_sentence_holder(root)
 root.bind('<Key>', key_stroke)
 
