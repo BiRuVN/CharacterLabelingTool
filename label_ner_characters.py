c1c2abb90e1e65a23a079cf8846930be9fa15373
BiRuVN <ngngquang99@gmail.com>
2021-03-29 00:04:30 +0700

    add save label

--- label_ner_characters.py
+++ label_ner_characters.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 
 root = Tk()
 root.title('Label coordinate')
-root.geometry('650x250')
+root.geometry('420x310')
 
 # # Define dataset path and labels here
 # label_folder = './data/training/cursor/seq_01/'
@@ -14,7 +14,6 @@ target_labels = {
     'address': 1
     # 'address': 3
 }
-
 
 sentence_index = 0
 
@@ -32,22 +31,35 @@ def select_all(event):
     event.widget.see(INSERT)
     return 'break'
 
-def load_sentence(sent):
-    global load, sentence_frame
-    load = [x for x in current_sentence]
+def get_sent_label(sent_idx):
+	if not os.path.exists('./labels/{}.txt'.format(str(sent_idx))):
+		with open('./labels/{}.txt'.format(str(sent_idx)), 'w+') as f:
+			f.close()
+	with open('./labels/{}.txt'.format(str(sent_idx)), 'r') as f:
+		l = f.read()
+		f.close()
+	return l
 
-    sentence_frame = Frame(height=200, width=650, bg='green')
+def load_sentence(sent, sent_label=''):
+    global sentence_frame, label_vars
+
+    if sent_label == '':
+    	sent_label = [0]*len(sent)
+
+    load = list(current_sentence)
+
+    sentence_frame = Frame(height=285, width=420, bg='green')
     sentence_frame.grid(row=0, column=0)
     sentence_frame.grid_propagate(False)
-    vars = []
+    label_vars = []
 
     print(current_sentence)
     print(load)
     row = 0
     column = 0
     for i in range(len(load)):
-        column = i % 50
-        row = int(i / 50)
+        column = i % 32
+        row = int(i / 32)
 
         char_frame = Frame(sentence_frame)
         char_frame.grid(row=row, column=column)
@@ -56,49 +68,20 @@ def load_sentence(sent):
         var = Text(char_frame, height=1, width=1)
         lbl.grid(row=0, column=0)
         var.grid(row=1, column=0)
-
         var.delete(1.0, END)
-        var.insert(1.0, '0')
+        var.insert(1.0, sent_label[i])
+
         var.bind("<Tab>", focus_next_widget)
         var.bind("<Shift-Tab>", focus_prev_widget)
         var.bind("<FocusIn>", select_all)
-        vars.append(var)
+        label_vars.append(var)
 
-    # with open(target_folder + current_sentence.split('.')[0] + '.txt', 'r') as label_file:
-    #     r = label_file.read()
-    #     r = r.strip().split(' ')
-    #     i = 0
-    #     for v in check_vars:
-    #         v.delete(1.0, END)
-    #         v.insert(1.0, '{} {}'.format(str(r[0+i*2]), str(r[1+i*2])))
-
-    #         if str(r[0+i*2]) == '-1' or str(r[1+i*2]) == -1:
-    #             render = ImageTk.PhotoImage(load)
-    #             sent_lbl.configure(image=render)
-    #             sent_lbl.image = render
-    #         else:
-    #             create_circle(x=int(r[0+i*2]), y=int(r[1+i*2]), color=label_colors[str(i)])
-
-    #         i +=1
-
-    chars = []
-
-    # for c in load:
-    #     char = Text(text_frame, height=2, width=2)
-    # sent_lbl.configure(text=sent)
-
-    return vars
+    return label_vars
 
 def render_sentence_holder(parent):
-    global current_sentence
+    global current_sentence, label_vars
 
-    # sentence_frame = Frame(parent)
-    # sentence_frame.grid(row=0, column=0)
-
-    # sent_lbl = Label(sentence_frame)
-
-    # sent_lbl.pack(side='top')
-    check_vars = load_sentence(current_sentence)
+    label_vars = load_sentence(current_sentence)
 
     progress_frame = Frame(parent)
     progress_frame.grid(row=1, column=0)
@@ -109,41 +92,21 @@ def render_sentence_holder(parent):
     
     return progress_lbl
 
-# def render_label_box(parent):
-#     label_box_frame = Frame(parent)
-#     label_box_frame.grid(row=0, column=1)
-    
-#     text_frame = Frame(label_box_frame)
-#     text_frame.pack(pady=20)
-
-#     vars = []
-#     for label in target_labels.keys():
-#         lbl = Label(text_frame, height=2, text=label)
-#         var = Text(text_frame, height=2, width=20)
-#         lbl.grid(row=target_labels[label], column=0)
-#         var.grid(row=target_labels[label], column=1)
-#         var.delete(1.0, END)
-#         var.insert(1.0, '-1 -1')
-#         vars.append(var)
-
-#     return vars
-
 def next_sent():
-    global sentence_index, sentences, current_sentence, sent_lbl, sentence_frame
+    global sentence_index, sentences, current_sentence, sentence_frame, label_vars
     global progress_lbl
     sentence_index += 1
     if sentence_index < len(sentences):
-        # with open(target_folder + current_sentence.split('.')[0] + '.txt', 'w') as label_file:
-        #     for v in check_vars:
-        #         s = v.get(1.0, END).strip().split(' ')
-        #         label_file.write('{} {}'.format(str(s[0]), str(s[1])))
-        #         label_file.write(' ')
+        with open('./labels/{}.txt'.format(str(sentence_index-1)), 'w+') as f:
+            for v in label_vars:
+                l = v.get(1.0, END).strip()
+                f.write(l)
+            f.close()
         
         current_sentence = sentences[sentence_index]
         for widget in sentence_frame.winfo_children():
             widget.destroy()
-        vars = load_sentence(sent=current_sentence)
-        vars = None
+        label_vars = load_sentence(sent=current_sentence, sent_label=get_sent_label(sentence_index))
         
     else:
         sentence_index -= 1
@@ -158,8 +121,8 @@ def prev_sent():
         current_sentence = sentences[sentence_index]
         for widget in sentence_frame.winfo_children():
             widget.destroy()
-        vars = load_sentence(sent=current_sentence)
-        vars = None
+        label_vars = load_sentence(sent=current_sentence, sent_label=get_sent_label(sentence_index))
+
     else:
         sentence_index += 1
 
@@ -177,6 +140,7 @@ def key_stroke(event):
 
 # get all sentences that need to be labeled
 sentences = []
+sentence_index = 0
 
 with open('sentences.txt', 'r') as f:
     sentences = [x.strip() for x in f.read().split('\n')]
@@ -184,19 +148,12 @@ with open('sentences.txt', 'r') as f:
 
 print('Got total {} sentences'.format(len(sentences)))
 
-label_path = './labels.txt'
-
-if not os.path.exists(label_path):
-    with open('labels.txt', 'w+') as f:
-        for sent in sentences:
-            f.write('1'*len(sent))
-            f.write('\n')
-        f.close()
+if not os.path.exists('./labels'):
+	os.mkdir('labels')
 
 current_sentence = sentences[sentence_index]
 sent_lbl = None
-load = None
-vars = load_sentence(current_sentence)
+# label_vars = load_sentence(current_sentence)
 progress_lbl = render_sentence_holder(root)
 root.bind('<Key>', key_stroke)
 
